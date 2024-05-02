f129b54674f4395d62f8b2c756ee21c4f38c7967
shankarsahi <shankarsahi.com@gmail.com>
2024-05-02 23:30:22 +0530

    Mostly All Done

--- DonationMsysteM/urls.py
+++ DonationMsysteM/urls.py
@@ -16,7 +16,115 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from HelpingHands import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("gallery/", views.gallery, name="gallery"),
+    path("login-admin/", views.login_admin.as_view(), name="login_admin"),
+    path("login-donor/", views.login_donor.as_view(), name="login_donor"),
+    path("login-volunteer/", views.login_volunteer.as_view(), name="login_volunteer"),
+    path("signup-donor/", views.signup_donor.as_view(), name="signup_donor"),
+    path("signup-volunteer/", views.signup_volunteer.as_view(), name="signup_volunteer"),
+    path("index-admin/", views.index_admin, name="index_admin"),
+    # admin dashboard
+    path("pending-donation/", views.pending_donation, name="pending_donation"),
+    path("accepted-donation/", views.accepted_donation, name="accepted_donation"),
+    path("rejected-donation/", views.rejected_donation, name="rejected_donation"),
+    path(
+        "volunteerallocated-donation/",
+        views.volunteerallocated_donation,
+        name="volunteerallocated_donation",
+    ),
+    path("donationrec-admin/", views.donationrec_admin, name="donationrec_admin"),
+    path(
+        "donationnotrec-admin/", views.donationnotrec_admin, name="donationnotrec_admin"
+    ),
+    path(
+        "donationdelivered-admin/",
+        views.donationdelivered_admin,
+        name="donationdelivered_admin",
+    ),
+    path("all-donations/", views.all_donations, name="all_donations"),
+    path("manage-donor/", views.manage_donor, name="manage_donor"),
+    path("new-volunteer/", views.new_volunteer, name="new_volunteer"),
+    path("accepted-volunteer/", views.accepted_volunteer, name="accepted_volunteer"),
+    path("rejected-volunteer/", views.rejected_volunteer, name="rejected_volunteer"),
+    path("all-volunteer/", views.all_volunteer, name="all_volunteer"),
+    path("add-area/", views.add_area.as_view(), name="add_area"),
+    path("edit-area/<int:pid>", views.edit_area.as_view(), name="edit_area"),
+    path("manage-area/", views.manage_area, name="manage_area"),
+    path("changepwd-admin/", views.changepwd_admin.as_view(), name="changepwd_admin"),
+    path("logout/", views.logoutView, name="logout"),
+    # view details
+    path(
+        "accepted-donationdetail/<int:pid>",
+        views.accepted_donationdetail.as_view(),
+        name="accepted_donationdetail",
+    ),
+    path(
+        "view-volunteerdetail/<int:pid>",
+        views.view_volunteerdetail.as_view(),
+        name="view_volunteerdetail",
+    ),
+    path("view-donordetail/<int:pid>", views.view_donordetail, name="view_donordetail"),
+    path(
+        "view-donationdetail/<int:pid>",
+        views.view_donationdetail.as_view(),
+        name="view_donationdetail",
+    ),
+    # donar dashboard
+    path("index-donor/", views.index_donor, name="index_donor"),
+    path("donate-now/", views.donate_now.as_view(), name="donate_now"),
+    path("donation-history/", views.donation_history, name="donation_history"),
+    path("profile-donor/", views.profile_donor.as_view(), name="profile_donor"),
+    path("changepwd-donor/", views.changepwd_donor.as_view(), name="changepwd_donor"),
+    # volunteer dashboard
+    path("index-volunteer/", views.index_volunteer, name="index_volunteer"),
+    path("collection-req/", views.collection_req, name="collection_req"),
+    path(
+        "donationrec-volunteer/",
+        views.donationrec_volunteer,
+        name="donationrec_volunteer",
+    ),
+    path(
+        "donationnotrec-volunteer/",
+        views.donationnotrec_volunteer,
+        name="donationnotrec_volunteer",
+    ),
+    path(
+        "donationdelivered-volunteer/",
+        views.donationdelivered_volunteer,
+        name="donationdelivered_volunteer",
+    ),
+    path("profile-volunteer/", views.profile_volunteer.as_view(), name="profile_volunteer"),
+    path("changepwd-volunteer/", views.changepwd_volunteer.as_view(), name="changepwd_volunteer"),
+    # vew details
+    path(
+        "donationdetail-donor/<int:pid>",views.donationdetail_donor,name="donationdetail_donor",
+    ),
+    path(
+        "donationrec-detail/<int:pid>",
+        views.donationrec_detail.as_view(),
+        name="donationrec_detail",
+    ),
+    path(
+        "donationcollection-detail/<int:pid>",
+        views.donationcollection_detail.as_view(),
+        name="donationcollection_detail",
+    ),
+    path("delete_donation/<int:pid>",views.delete_donation,name="delete_donation"),
+    path("delete_volunteer/<int:pid>",views.delete_volunteer,name="delete_volunteer"),
+    path("delete_area/<int:pid>",views.delete_area,name="delete_area"),
+    path("delete_donor/<int:pid>",views.delete_donor,name="delete_donor"),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
