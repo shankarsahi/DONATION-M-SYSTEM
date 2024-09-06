dfd40290e218bc11ce4cecf79cf6308f8d13b1c9
shankarsahi <shankarsahi.com@gmail.com>
2024-09-06 18:23:58 +0530

    commited for Deploy

--- DonationMsysteM/settings.py
+++ DonationMsysteM/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-z^(%lm__l=h*$&*tbvn=fm2ro^oj$axj^!9%1!h80fp1tv9jlc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
-
+ALLOWED_HOSTS = [".vercel.app",".now.sh","127.0.0.1","localhost"]
 
 # Application definition
 
@@ -121,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URLS = BASE_DIR/ 'static'
+# STATIC_URLS = BASE_DIR/ 'static'
+STATICFILES_DIRS=os.path.join(BASE_DIR,"static"),
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
 
 
 
